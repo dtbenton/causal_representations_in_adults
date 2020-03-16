ae15d7c13256278def3dbb159d588b876aaab2c2
Deon T. Benton <dtbenton@andrew.cmu.edu>
2020-03-16 18:07:17 -0400

    s

--- exp_1/exp_1_rscript.R
+++ exp_1/exp_1_rscript.R
@@ -29,36 +29,55 @@ options(scipen=9999)
 
 # load data
 D = read.csv(file.choose(), header = TRUE)
-D = as.data.frame(D[1:64,])
 
 # reorder columns
 D = as.data.frame(D[,c(1,2,3,9,5,7,4,10,6,8,11,12)])
 
 # reshape the data
-D_tall = reshape(D, varying = 3:10, v.names = "measure", timevar = "condition", idvar = "ID", new.row.names = 1:512, direction = "long")
+D_tall = reshape(D, varying = 3:10, v.names = "measure", timevar = "condition", 
+                 idvar = "ID", 
+                 new.row.names = 1:512, direction = "long")
 
 # order data
 D_tall = D_tall[order(D_tall$ID),]
 
 
-# add q.type.cat column
-D_tall$q.type.cat = as.factor(rep(c(1:2), each = 4, times = 64))
+
+# convert all categorical variables to 'factors'
+D_tall$q.type.cat = as.factor(rep(c(0,1), each = 4, times = 64))
+D_tall$q.type.cat = revalue(x = as.factor(D_tall$q.type.cat),
+                            c("0"="Perceptual", "1"="Causal"))
+
+D_tall$q.type = revalue(x = as.factor(D_tall$q.type), 
+                                                c("0" = "Perceptual_First", 
+                                                  "1"="Causal_First")) # this corresponds to whether participants
+                                                                       # were asked the causal or perceptual question
+                                                                       # first.
+
+D_tall$exp = revalue(x = as.factor(D_tall$exp),
+                       c("0" = "Kaily", "1" = "Marina"))
 
 
-# set appropriate factor variables in "wide" data
-D$group = as.factor(D$group)
-D$q.type = as.factor(D$q.type)
+D_tall$group = revalue(x = as.factor(D_tall$group),
+                     c("0" = "Red", "1" = "Blue"))
 
-# set appropriate factor variables in "tall" data
-D_tall$condition = as.factor(D_tall$condition)
-D_tall$group = as.factor(D_tall$group)
-D_tall$q.type = as.factor(D_tall$q.type)
-D_tall$q.type.cat = as.factor(D_tall$q.type.cat)
-D_tall$measure.2 = (100-D_tall$measure)
+
+D_tall$test_trials = as.factor(rep(c(1:4), each = 1, 
+                                   times = 128))
+D_tall$test_trials = revalue(x = as.factor(D_tall$test_trials),
+                       c("1" = "gBgR", "2" = "gRgB",
+                         "3" = "gBRg", "4" = "gRBg"))
+
+
+
+# drop unnecessary columns and reorder 'D_tall'
+D_tall$row.names = NULL
+D_tall$condition = NULL
+D_tall = D_tall[,c(1:5,7,6)]
 
 
 # names of column in 'D_tall'
-"ID"        "exp"       "q.type"    "group"     "condition" "measure"   "conditon" 
+# "ID"         "exp"        "group"      "q.type"     "q.type.cat" "measure" 
 
 
 ########################################################
@@ -68,33 +87,50 @@ D_tall$measure.2 = (100-D_tall$measure)
 ########################################################
 
 ## NORMALITY CHECKS
-
 par(mfrow=c(4,2)) 
-for (ii in 1:8)  hist(D_tall$measure[D_tall$condition==ii], breaks=5)
+for(i in c("Perceptual","Causal")){
+  for(j in c("gBgR","gRgB","gBRg","gRBg")){
+    hist(D_tall$measure[D_tall$test_trials==j], breaks=5)
+  }
+}
 par(mfrow=c(1,1)) 
 
+
 # formal test of normality
-. = rep(0,8)
-for(i in 1:8) {
-  shap.calc = shapiro.test(D_tall$measure[D_tall$condition==i])
-  shapiro.ps[i] = shap.calc$p.value
+shapiro.ps = rep(0,8)
+for(i in c("Perceptual","Causal")){
+  for(j in c("gBgR","gRgB","gBRg","gRBg")){
+    shap.calc = shapiro.test(D_tall$measure[D_tall$test_trials==j])
+    shapiro.ps[i] = shap.calc$p.value
+  }
 }
+
 
 ## HOMOSKEDASTICITY CHECKS
 # plot the boxplots
-# perceptual
-boxplot(D_tall$measure[D_tall$condition==c(1:4)]~D_tall$condition[D_tall$condition==c(1:4)])
+init_box_plots = ggplot(D_tall, aes(x = test_trials, 
+                                 y = measure)) + geom_boxplot() + facet_wrap(~q.type.cat)
+init_box_plots
 
-# causal
-boxplot(D_tall$measure[D_tall$condition==c(5:8)]~D_tall$condition[D_tall$condition==c(5:8)])
+par(mfrow=c(4,2))
+for (i in c("Perceptual","Causal")){
+  for(j in c("gBgR","gRgB","gBRg","gRBg")){
+    boxplot(D_tall$measure[D_tall$q.type.cat==i & D_tall$test_trials==j], breaks=21)
+  }
+} 
+par(mfrow=c(1,1))
 
 # formal test of equal variance
 # perceptual
-leveneTest(D_tall$measure[D_tall$condition==c(1:4)], as.factor(D_tall$condition[D_tall$condition==c(1:4)]), center=median) # used 'median' because it's a better measure of central tendency given the non-normality
+levene_test_vec = rep(0,2)
+for (i in c("Perceptual","Causal")){
+    calc = leveneTest(D_tall$measure[D_tall$q.type.cat==i], 
+                      as.factor(D_tall$test_trials[D_tall$q.type.cat==i]),
+                      center = median)$"Pr(>F)"[[1]] 
+    levene_test_vec[i] = calc
+}
+levene_test_vec
 
-
-# causal
-leveneTest(D_tall$measure[D_tall$condition==c(5:8)], as.factor(D_tall$condition[D_tall$condition==c(5:8)]), center=median) # used 'median' because it's a better measure of central tendency given the non-normality
 
 ## ASSUMPTION CHECK NOTES ##
 # Despite the fact that there is no evidence of heteroskedasticity, because
@@ -102,6 +138,55 @@ leveneTest(D_tall$measure[D_tall$condition==c(5:8)], as.factor(D_tall$condition[
 # testing will be used to estimate confidence intervals and for hypothesis checking
 
 
+####################
+####################
+# GLOBAL FUNCTIONS #
+####################
+####################
+boot_func = function(x,y,t1, data){
+  bbB.lme.fit  <- function(d,i) {  # this worked for lme boot!
+    d <- d[i,]
+    # create new Subject id so each is unique
+    #d$Subject <- 1:dim(d)[1]#
+    thecoef <- tryCatch({bootfm.lme <- lme(measure ~ test_trial, data=d,
+                                           random=~1|ID, subset=t1)
+    fixef(bootfm.lme)},
+    error = function(e) {print(e)
+      return(rep(NA,length(fixef(bootfm.lme))))})
+    #    browser()
+    return(thecoef)[2]
+  }
+  
+  set.seed(2017)
+  bbB.lme.Bootobj = boot(data, bbB.lme.fit , R=4000)
+  bbB.lme.Bootobj
+}
+
+
+
+################################################
+################################################
+# WHETHER TO INCLUDE FIXED- VS. RANDOM EFFECTS #
+################################################
+################################################
+
+# intercept only model with random effects of subjects
+model_1 = lme(measure ~ 1, random=~1|ID,
+              data=D_tall)
+
+model_2 = lme(measure ~ 1, random=~1|exp/ID,
+              data=D_tall)
+
+model_3 = lm(measure~1, data = D_tall)
+
+anova(model_1,model_2)
+anova(model_1,model_3) # include random effects
+anova(model_2,model_3)
+
+
+# This analysis indicates that a model in which each deviation of each subject's intercept
+# around the group intercept is the best model. Here, this is model_2. Thus, in all subsequent
+# analyses random-effect intercepts will be included for subjects.
 
 ########################################################
 ########################################################
@@ -117,7 +202,7 @@ leveneTest(D_tall$measure[D_tall$condition==c(5:8)], as.factor(D_tall$condition[
 #### PRELIMINARY ANALYSIS ####
 ##############################
 # analysis to determine effect of question type and 
-lme.fit.prelim = lme(measure~q.type+group+q.type:group, random=~1|ID, data=D_tall)
+lme.fit.prelim = lme(measure~(q.type+group)^2, random=~1|ID, data=D_tall)
 anova.lme(lme.fit.prelim)
 r2(lme.fit.prelim)
 ## PRELIMINARY ANALYSIS NOTES
